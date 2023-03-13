2af2184c707dece1504a482c45ba26658d0649b7
Geert van der Cruijsen <geert@vdcruijsen.net>
2023-03-13 21:12:02 +0100

    updated test with correct list

--- tests/availability/availability.sh
+++ tests/availability/availability.sh
@@ -2,7 +2,7 @@
 count=0
 until false
 do
-	curl -w @curl-format.txt -s -d '{"name": "test'$count'","listId": "d88ac7c552394cdea365d3f1c4f2e105","state": "todo"}' -H "Content-Type: application/json" -X POST https://app-api-kihrxlbnlw6v2.azurewebsites.net/lists/d88ac7c552394cdea365d3f1c4f2e105/items -o /dev/null
+	curl -w @curl-format.txt -s -d '{"name": "test'$count'","listId": "28903c5cb72e40ab9773328bbabe1ebe","state": "todo"}' -H "Content-Type: application/json" -X POST https://app-api-iszsinkujhif6.azurewebsites.net/lists/28903c5cb72e40ab9773328bbabe1ebe/items -o /dev/null
 	((count++))
 	echo "$count $OUTPUT"
     sleep 1
